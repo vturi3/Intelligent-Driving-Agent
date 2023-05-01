e9770bac4591be125d1c2bb687cd3e502584a0a9
MattiaMarseglia <m.marseglia1@studenti.unisa.it>
2023-05-01 13:47:51 +0200

    fix a merge bug

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -639,7 +639,7 @@ class BehaviorAgent(BasicAgent):
         # if dir == "right":
         # #     print("láuto che non mi fa sorpassare è: ", com_vehicle)
             #input()
-        if obj_to_s and com_vehicle:
+        if obj_to_s:
             if not com_vehicle_state or (com_vehicle_state and com_vehicle_distance>80):
                 # print('STO PER STARTARE IL SORPASSO, IL VEICOLO DISTA: ', com_vehicle_distance, "ed è: ", com_vehicle)
                 # input()
